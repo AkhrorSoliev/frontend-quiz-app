5634f272b319b4ff6612287252baf93940b39fe8
akhrorsoliev <akhsoliev@gmail.com>
2024-06-01 11:18:45 +0500

    Test part made responsive full

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,20 +1,81 @@
-import React from "react";
+import { useState } from "react";
 
 function Test({ questions: { questions, title, color, icon } }) {
+  const [answeredQuestions, setAnsweredQuestions] = useState(1);
+  const [questionIndex, setQuestionIndex] = useState(0);
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+  const [answerStatus, setAnswerStatus] = useState(null);
+  const [statusDisabeled, setStatusDisabled] = useState(false);
+
   return (
     <div className="test-container">
       <div className="test-content">
-        <p className="test-description">Question 6 of 10</p>
-        <h2 className="test-title">
-          Which of these color contrast ratios defines the minimum WCAG 2.1
-          Level AA requirement for normal text?
-        </h2>
+        <p className="test-description">
+          Question {answeredQuestions} of {questions.length}
+        </p>
+        <h2 className="test-title">{questions[questionIndex].question}</h2>
 
         <div className="test-proccess-container">
-          <div className="test-proccess"></div>
+          <div
+            className="test-proccess"
+            style={{
+              width: (answeredQuestions / questions.length) * 100 + "%",
+            }}
+          ></div>
         </div>
       </div>
-      <div className="test-questions"></div>
+      <div className="test-questions">
+        <form>
+          <ul className="test-list">
+            {questions[questionIndex].options.map((option, index) => {
+              const alphabet = String.fromCharCode(index + 65);
+              let className = "";
+              if (answerStatus === "correct" && option === selectedAnswer) {
+                className = "correct";
+              } else if (answerStatus === "incorrect") {
+                if (option === selectedAnswer) {
+                  className = "incorrect";
+                }
+                if (option === questions[questionIndex].answer) {
+                  className = "correct";
+                }
+              }
+
+              return (
+                <li key={option}>
+                  <label className={`test-label ${className}`}>
+                    <span className="test-letter">{alphabet}</span>
+                    <input
+                      onChange={() => setSelectedAnswer(option)}
+                      type="radio"
+                      name="option"
+                      disabled={statusDisabeled}
+                    />
+                    <span className="test-text">{option}</span>
+
+                    {/* icon */}
+                    <img
+                      className="test-icon-correct"
+                      src="../assets/icon-correct.svg"
+                      alt="icon"
+                      width={40}
+                      height={40}
+                    />
+                    <img
+                      className="test-icon-incorrect"
+                      src="../assets/icon-incorrect.svg"
+                      alt="icon"
+                      width={40}
+                      height={40}
+                    />
+                  </label>
+                </li>
+              );
+            })}
+          </ul>
+          <button className="btn test-btn">Submit</button>
+        </form>
+      </div>
     </div>
   );
 }
